489b6fb5a7a1bc251f8aadeb37d1cabf33ca0504
Raphael <int3cp4y>
2018-03-28 16:31:47 -0300

    Criando referencias entre sites.

--- src/resources/views/forms/components/select.blade.php
+++ src/resources/views/forms/components/select.blade.php
@@ -1,12 +1,17 @@
 @php ($label = (isset($label)?$label:null))
 @php ($errors = (isset($errors)?$errors:null))
-@component("crud-rails::forms.formField",["name" => $name, "label" => $label, "errors" => $errors])
+@component("crud-rails::forms.formField",["name" => $field_config->name, "label" => $field_config->label, "errors" => $errors])
 
     <div class="{{isset($newAction)?"input-group":""}}">
-        <select class='form-control' id='{{$name}}' name='{{$name}}'>
+        <select class='form-control' id='{{$field_config->name}}' name='{{$field_config->name}}'>
             <option value=""></option>
-            @foreach( $options as $key => $option )
-                <option value="{!! $key  !!}" {{((old($name) != null && old($name) == $key) || (isset($model) && $model[$name] == $key))?'selected="selected"':''}}>{!! $option !!}
+            @foreach( $field_config->options as $key => $option )
+                <option
+                        value="{!! $key  !!}"
+                        {{(
+                            (old($field_config->name) != null && old($field_config->name) == $key) ||
+                            (isset($field_config->value) && $field_config->value == $key))?'selected="selected"':''}}>
+                    {!! $option !!}
                 </option>
             @endforeach
         </select>
