5adb71429aa7adddddc8d806aa5f3ffaa6233109
Raphael <int3cp4y>
2018-04-18 16:27:22 -0300

    Corrigindo o botão de salvar

--- src/resources/views/forms/components/submit.blade.php
+++ src/resources/views/forms/components/submit.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <div class="col-12 text-center p-5">
         <button type="submit" class="btn btn-lg btn-success">
-            <i class="fas fa-cloud-upload-alt"></i> {{trans('crud-rails.actions.save')}}
+            <i class="fas fa-cloud-upload-alt"></i> Salvar
         </button>
     </div>
 </div>
